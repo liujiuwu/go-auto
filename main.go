49b78af41dfa1302a3696a0ddeecf68b891d6795
liujiuwu <ljw@szzfht.com>
2014-11-12 11:25:35 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "os/exec"
+
+    "github.com/Unknwon/macaron"
+)
+
+func main() {
+    m := macaron.Classic()
+    m.Get("/auto", autoHandler)
+
+    log.Println("Server is running...")
+    log.Println(http.ListenAndServe("0.0.0.0:4000", m))
+}
+
+func autoHandler(ctx *macaron.Context) string {
+    output, _ := exec.Command("cmd.exe", "/c", "ping", "127.0.0.1").Output()
+    log.Println(string(output))
+
+    return "the request path is: " + ctx.Req.RequestURI
+}
